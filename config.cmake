ffaa5ce5eb9606457813c7096b29fcad50c34772
programming101dev <113087924+programming101dev@users.noreply.github.com>
2026-07-31 08:50:18 -0700

    Add complete public API unit-test coverage

--- config.cmake
+++ config.cmake
@@ -46,5 +46,5 @@ set(p101_c_facts_LINK_LIBRARIES
         p101_tool_event
         p101_env
         p101_c
-        p101_posix
+        p101_filesystem
 )
--- src/project.c
+++ src/project.c
@@ -1,7 +1,7 @@
 #include "p101_c_facts/project.h"
 #include <p101_c/p101_stdio.h>
 #include <p101_c/p101_string.h>
-#include <p101_posix/p101_unistd.h>
+#include <p101_filesystem/filesystem.h>
 #include <unistd.h>
 
 enum
